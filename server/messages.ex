39b909f61da955291481a6ac7e249a62a2755102
Luke Segars <luke@theironyard.com>
2016-04-12 02:50:15 -0400

    Added some comments.

--- server/messages.ex
+++ server/messages.ex
@@ -1,4 +1,5 @@
 defmodule Messages do
+    # This module's mainly just around to practice require'ing modules
 
     def announce do
         "Tis I! I've made it!" 
--- server/server.ex
+++ server/server.ex
@@ -3,6 +3,8 @@ defmodule ChatServer do
     # between requests. This could also be expanded out to keep track of much more than
     # just strings.
     require Messages
+    # TODO: soon I'll also import an adapter here that will expose Phoenix connections
+    # (websockets to browsers, I think?).
     
     def main() do
         IO.puts "Hello, world!"
@@ -15,6 +17,15 @@ defmodule ChatServer do
         IO.puts Messages.announce()
     end
     
+    # get_msg has two clauses, and Elixir will try to match in the order that they're defined.
+    # That means that effectively I think this is kinda like:
+    #
+    # def get_msg(op, param) do
+    #   if (op === :ok) return "success!";
+    #   else return "not correct...";
+    # end
+    #
+    # ...in a procedural language.
     def get_msg(:get, param) do
         {:ok, "success!"}
     end
